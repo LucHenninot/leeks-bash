90aed6ca8c485e1e3a0b3ed19a205fef10893546
Luc Henninot <luc.henninot@gmail.com>
2021-11-08 16:14:17 +0100

    tryGarden.sh: added fight delay

--- tryGarden.sh
+++ tryGarden.sh
@@ -37,6 +37,8 @@ fi
 function tryLeek() {
 	echo -n "Trying $1: "
 	curl -sS -H "Content-Type: application/x-www-form-urlencoded" -H "Authorization: Bearer ${token}" -d "leek_id=$id_leek&target_id=$2&seed=$((RANDOM*RANDOM))" -X POST ${SITE}/garden/start-solo-challenge | jq -r .fight
+	# Don't overload the server
+	sleep 1
 }
 
 tryLeek $P1 $I1
